8fa87644c5eaca5db950e9cf7ae2e81d8ea09a20
Newazgddo <85012072+gitbyhabib@users.noreply.github.com>
2026-02-05 16:06:56 +0600

    date and facebook link updated

--- src/components/client/Footer.vue
+++ src/components/client/Footer.vue
@@ -6,7 +6,7 @@
           <h5 class="text-xl font-bold text-blue-400 mb-4">Al Sharfaraj Overseas</h5>
           <p class="text-gray-400">Providing world-class visa, travel, and manpower solutions from the heart of Bangladesh.</p>
           <div class="flex mt-4 space-x-4">
-            <a href="#" class="text-gray-400 hover:text-white transition duration-300"><i class="fab fa-facebook-f text-2xl"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=61580208326487" class="text-gray-400 hover:text-white transition duration-300"><i class="fab fa-facebook-f text-2xl"></i></a>
             <a href="#" class="text-gray-400 hover:text-white transition duration-300"><i class="fab fa-twitter text-2xl"></i></a>
             <a href="#" class="text-gray-400 hover:text-white transition duration-300"><i class="fab fa-linkedin-in text-2xl"></i></a>
           </div>
@@ -34,7 +34,7 @@
       </div>
       <hr class="border-gray-700 my-8">
       <div class="text-center text-gray-500">
-        <p>© 2025 Al Sharfaraj Overseas. All Rights Reserved.</p>
+        <p>© {{ new Date().getFullYear() }} Al Sharfaraj Overseas. All Rights Reserved.</p>
       </div>
     </div>
   </footer>
